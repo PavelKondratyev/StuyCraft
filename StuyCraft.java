3ca46a3cb4a2b0cbf2d750703fc9acf909cc87be
PavelKondratyev <paulkondratyev@hotmail.com>
2014-10-23 22:08:20 +0000

    Made an initialize game function, added framework for enemy and boss encounters

--- StuyCraft.java
+++ StuyCraft.java
@@ -2,38 +2,71 @@ import java.util.*;
 
 
 public class StuyCraft{
-    
 
-    public static void main(String[]args){
-	System.out.println("\n\nWelcome to StuyCraft\n\nWhere myths come to life! \n\nPlease choose your character: \nSabetour: Caster who specializes in debuff. \nDualBlade: Brutal fighter that slashes enemies \n \n"); 
+    //Instance Variables 
+    private int wave, level, money;
+    private boolean gameOver, bossLevel;
+    private Character player, enemy;
+    //There are 3 waves with 4 levels each - each 4th level is the boss match
+    //gameOver is used to signify if the game is over or not, bossLevel signifies if the current level is a boss level
+
+
+
+    //Constructor for StuyCraft
+    public StuyCraft(){
+	wave = 1;
+	level = 1;
+	money = 100;
+	gameOver = false;
+	initGame();
+    }
+
+    public void initGame(){
+	String welcomeMessage = "";
+	welcomeMessage+= "============================\n";
+	welcomeMessage+= "Welcome to StuyCraft!\n";
+	welcomeMessage+= "Where myths come to life!\n";
+	welcomeMessage+= "Please choose your character(case sensitive) :\n";
+	welcomeMessage+= "\tSabetour: Caster who specializes in debuff\n";
+	welcomeMessage+= "\tDualBlade: Brutal fighter that slashes enemies and\n";
+	welcomeMessage+= "\tRanger: Archer with the ability to critically strike\n";
+	welcomeMessage+= "============================";
+      	System.out.println(welcomeMessage);
+	
 	String Choice;
 	Scanner Choose = new Scanner(System.in); //Choosing options in cmd
-	Choice = Choose.next(); //Waiting in cmd for response
-	
-	if ((Choice).equals("Sabetour") || (Choice).equals("DualBlade") ){
-	    System.out.println( Choice + " " + "has been initialized");
-	}
-	
-	else{
-	    System.out.println( Choice +" "+ "is not a recongnized keyword");
-	    System.out.println("Error: Character Not Recognized \n Please restart StuyCraft");
-	    System.exit(1); //Ends the CMD.
-	}	    
-
-	System.out.println("\n\nWould you like to start a new game or load a save file? \n\nNew Game          Load Game\n\n");
-
 	Choice = Choose.next();
 
-	if ((Choice).equals ("Load Game")){ //Save 
-	    System.out.println("Enter save file keyword\n");
+	while(!((Choice).equals("Sabetour") || (Choice).equals("DualBlade") || (Choice).equals("Ranger"))){
+	    System.out.println("Invalid selection, please try again");
+	    System.out.println("Select your character:");
 	    Choice = Choose.next();
-	    if ((Choice).equals( "STAGE1") || (Choice).equals( "STAGE2")){
-		System.out.println(Choice +" "+ "has been initialized");
-	    }
-	    else{
-		System.out.println(Choice + "is not a recognized keyword \n Please try again.");
-		System.exit(1); //Ends the CMD
-	    }
+	}
+	  
+	System.out.println( Choice + " " + "has been initialized");
+	if(Choice.equals("Sabetour")){
+	    player = new Sabetour();
+	}
+	else if(Choice.equals("DualBlade")){
+	    player = new DualBlade();
+	}
+	else if(Choice.equals("Ranger")){
+	    player = new Ranger();
 	}
     }
+
+    public static void enemyEncounter(){
+	enemy = new Skeleton();
+
+    }
+
+    public static void bossEncounter(){
+    }
+    
+    public static void main(String[] args){
+
+    }
+	
+
+	
 }

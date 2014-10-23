5cab98c33eda51f728aca68664afc6df4c771431
pmMeYourName <pmMeYourName@users.noreply.github.com>
2014-10-23 09:33:50 -0400

    Delete Game_funtimes.java

--- Game_funtimes.java
+++
@@ -1,7 +0,0 @@
-public class Game_funtimes {
-  public static void main( String args[] ){
-    System.out.println( "Make me a contributor please" );
-    System.out.println( "Thank you" );
-    
-  }
-}

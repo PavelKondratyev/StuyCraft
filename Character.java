445ce467643ea0341ffd4c3dd8d7e4ab8caf0c6d
PavelKondratyev <paulkondratyev@hotmail.com>
2014-10-26 22:26:22 +0000

    Got rid of default special attack methods cause they aren't necessary

--- Character.java
+++ Character.java
@@ -1,18 +1,9 @@
 class Character{
     protected String Name;
-    protected int HP, MP, Attack, SpAttack, Defense, SPDefense, Accuracy;
+    protected int HP, maxHP , MP, maxMP , Attack, SPAttack, Defense, SPDefense, Accuracy;
     protected double Speed;
  
     
-    public Character( String name ){
-	Name = name;
-	HP = 100;
-	Strength = 10;
-	Defense = 10;
-	Speed = 1.0;
-	Accuracy = 8;
-    }
-
     public boolean isAlive(){
 	if(HP <= 0){
 	    return false}
@@ -29,7 +20,6 @@ class Character{
 		}
     }
 
-
     public int  calcDamage( Character target ){
 	int Damage = 0;
 	if (missHit() == false){
@@ -42,25 +32,10 @@ class Character{
 	return Damage;
     }
     
-    public int calcSPDamage( Character target ){//calculates damage from special attack
-	int Damage = 0;
-	if (missHit() == false){
-	    Damage += SPAttack - target.SPDefense;
-	    System.out.println( name + " attacked " + target.Name + " for " + Damage + " damage!");
-	}
-	else {
-	    System.out.println( name + "'s attack missed!");
-	}
-	return Damage;
-    }
-    
     public void attack( Character target ){
 	int Damage = calcDamage(target);
 	target.HP -= Damage;
     }
 
-    public void SPAttack( Character target ){
-	int Damage = calcSPDamage(target);
-	target.HP -= Damage;
 
 }

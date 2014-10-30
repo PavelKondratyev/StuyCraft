2aaa95d7e6f1ec9a2fef3d6bd2aea7ad9b834c24
pmMeYourName <pmMeYourName@users.noreply.github.com>
2014-10-29 22:52:19 -0400

    the shop is back

--- StuyCraft.java
+++ StuyCraft.java
@@ -63,6 +63,63 @@ public class StuyCraft{
 	}
     }
 
+        public static void Shop() {
+        	boolean shop = true;
+        	boolean hpShop = true;
+                boolean mpShop = true;
+        	Scanner buy = new Scanner(System.in);
+        	while ( shop == true) {
+        		System.out.println("Hey listen! It's dangerous to go alone! Buy something!");
+        		System.out.println("You have " + hpPots + " HP potions.");
+        		System.out.println("You have " + mpPots + " MP potions.")
+        		do {
+        			System.out.println("HP Potion | 25 moneys (Type a number)");
+        			if ( buy.hasNextInt() ) {
+	        			buy.nextInt();
+	        			if ( ( buy * 25 ) > money ) {
+	        				System.out.println("Fool! You don't have enough moneys!");
+	        				System.out.println("Try again!");
+	        			}
+	        			else {
+	        				System.out.println("Success! You bought " + buy + " HP pot(s).")
+	        				money -= ( buy * 25 );
+	        				hpPots += buy;
+	        				hpShop = false;
+	        			}
+	        		else {
+	        			System.out.print("Invalid response");
+	        		}
+	        		}
+	        		} while ( hpShop = true );
+	        	do {
+	        		System.out.println("MP Potion | 10 moneys (Type a number)");
+	        		if ( buy.hasNextInt() ) {
+	        			buy.nextInt();
+	        			if ( ( buy * 10 ) > money ) {
+	        				System.out.println("Fool! You don't have enough moneys!");
+	        				System.out.println("Try again!");
+	        			}
+	        			else {
+	        				System.out.println("Success! You bought " + buy + " MP pot(s).")
+	        				money -= ( buy * 10 );
+	        				mpPots += buy;
+	        				mpShop = false;
+	        			}
+	        		else {
+	        			System.out.print("Invalid response");
+	        		}
+	        		}
+	        		} while ( mpShop = true );
+	        	System.out.println("Type in exit to leave or anything else to stay!");
+	        	Scanner exit = new Scanner(System.in);
+	        	exit.next();
+	        	if ( exit.equalsIgnoreCase( "exit" ) ) {
+	        		shop = false;
+	        	}
+        	System.out.println("Have a nice day!");
+               }
+        }
+
     public void enemyEncounter(){
 	enemy = new Skeleton( wave );
 	Scanner move = new Scanner(System.in);
